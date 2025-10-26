be922074d7fc8d3d3baafe98a98030d581df1746
meko iz <mecockney624@gmail.com>
2025-10-26 16:57:42 +0900

    サインアウト処理の実装

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -2,26 +2,22 @@
 
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
 import { createClient } from '@/utils/supabase/server';
 
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
 
   if (error) {
-    console.log(error);
+    console.error('ログインエラー:', error.message);
     redirect('/error');
   }
 
+  // cookieがブラウザにセットされた状態でリダイレクト
   revalidatePath('/', 'layout');
   redirect('/workspace');
 }
